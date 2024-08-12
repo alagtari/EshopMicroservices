2f7ee1cb44107fe4c07eb471762f191748c9e445
alagtari <alagtari@yahoo.fr>
2024-08-12 09:22:26 +0100

    add missing files

---
+++ src/Services/Basket/Basket.Api/Program.cs
@@ -0,0 +1,50 @@
+
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly;
+builder.Services.AddMediatR(options =>
+{
+    options.RegisterServicesFromAssembly(assembly);
+    options.AddOpenBehavior(typeof(ValidationBehavior<,>));
+});
+
+builder.Services.AddMarten(options =>
+{
+    options.Connection(builder.Configuration.GetConnectionString("Database")!);
+    options.Schema.For<ShoppingCart>().Identity(x => x.UserName);
+}).UseLightweightSessions();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
+{
+    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+})
+.ConfigurePrimaryHttpMessageHandler(() =>
+{
+    var handler = new HttpClientHandler
+    {
+        ServerCertificateCustomValidationCallback =
+        HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+    };
+
+    return handler;
+});
+
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddCarter();
+
+var app = builder.Build();
+
+app.MapCarter();
+app.UseExceptionHandler(options => { });
+
+app.Run();
